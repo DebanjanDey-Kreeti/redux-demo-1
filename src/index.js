099c12233d553ea6618c2947c1c2425f367359d1
DebanjanDey-Kreeti <debanjan.dey@kreeti.com>
2019-07-11 18:44:32 +0530

    1st set of changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-import { createStore, bindActionCreators } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = function(state, action) {
-  if(action.type === "INC") {
-    return state + action.payload;
-  }
-  if(action.type === "DEC") {
-    return state - action.payload;
-  }
-  if(action.type === "MUL") {
-    return state * action.payload;
+const userReducer = (state={}, action) => {
+  switch(action.type) {
+    case "CHANGE_NAME": {
+      state = {...state, name: action.payload};
+      break;
+    }
+    case "CHANGE_AGE": {
+      state = {...state, age: action.payload};
+      break;
+    }
+    default : {
+    }
   }
   return state;
-}
+};
 
-const store = createStore(reducer, 0);
+const tweetsReducer = (state=[], action) => {
+  return state;
+};
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer,
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
   console.log("store changed", store.getState())
 });
 
-store.dispatch({ type: "INC", payload: 1 });
-store.dispatch({ type: "INC", payload: 25 });
-store.dispatch({ type: "DEC", payload: 110 });
-store.dispatch({ type: "MUL", payload: -72 });
+store.dispatch({ type: "CHANGE_NAME", payload: "Will" });
+store.dispatch({ type: "CHANGE_AGE", payload: 35 });
+store.dispatch({ type: "CHANGE_AGE", payload: 40 });
